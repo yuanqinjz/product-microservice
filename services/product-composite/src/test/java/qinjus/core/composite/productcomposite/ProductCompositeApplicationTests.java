13add57d757e671631ca2897f32c31a60d6c8faa
Justin Qin <yuanqinjz@gmail.com>
2024-04-26 09:57:36 -0500

    Create the gradle multi-project, use the multi-project approach to keep things simple

---
+++ services/product-composite/src/test/java/qinjus/core/composite/productcomposite/ProductCompositeApplicationTests.java
@@ -0,0 +1,13 @@
+package qinjus.core.composite.productcomposite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/review-service/src/test/java/qinjus/core/review/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package qinjus.core.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
